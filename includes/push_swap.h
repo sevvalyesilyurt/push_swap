ecce559da316da8c201ace369c7a5a40024cbbca
Ahmet Elman <ahelman@student.42kocaeli.com.tr>
2026-04-19 15:54:53 +0300

    fonskiyon prototipleri güncellendi

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -8,20 +8,51 @@ typedef struct s_node
 	struct s_node	*next;
 }					t_node;
 
+// bench kısmı struct olucak
+typedef struct s_bench
+{
+	float			disorder;
+	char			*strategy;
+	int				total_ops;
+	int				sa;
+	int				sb;
+	int				ss;
+	int				pa;
+	int				pb;
+	int				ra;
+	int				rb;
+	int				rr;
+	int				rra;
+	int				rrb;
+	int				rrr;
+	int				is_bench;
+}					t_bench;
+
 // stack yapısı için gerekli fonksiyonlar
 t_node				*new_node(long number);
 t_node				*last_node(t_node *lst);
 int					size_node(t_node *node);
+void				free_stack(t_node **node);
 void				add_back(t_node **node, t_node *new);
 void				add_front(t_node **node, t_node *new);
 
-// stack'ten stack'e push için kullanacağımız fonksiyonlar
-void				pb(t_node **a, t_node **b);
-void				pa(t_node **b, t_node **a);
-void				just_pb(t_node **a, t_node **b);
-void				just_pa(t_node **b, t_node **a);
+// stack'teki işlemler için kullanacağımız fonksiyonlar
+void				pb(t_node **a, t_node **b, int print);
+void				pa(t_node **b, t_node **a, int print);
+// swap
+void				sa(t_node **a, int print);
+void				sb(t_node **b, int print);
+void				ss(t_node **a, t_node **b, int print);
+// reverse rotate
+void				rrr(t_node **a, t_node **b, int print);
+void				rra(t_node **a, int print);
+void				rrb(t_node **b, int print);
+// rotate
+void				rr(t_node **a, t_node **b, int print);
+void				ra(t_node **a, int print);
+void				rb(t_node **b, int print);
 
 // utils kısmı
-void	error_exit(t_node **a);
+void				error_exit(t_node **a);
 
 #endif
