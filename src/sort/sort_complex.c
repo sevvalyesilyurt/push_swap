cd9a334c1eff1cf7804671782d4e49879960157e
sevvalYesilyurt41 <sevvalyesilyurt4@gmail.com>
2026-04-28 23:57:13 +0300

    kütüphane eksikti complexte

--- src/sort/sort_complex.c
+++ src/sort/sort_complex.c
@@ -6,11 +6,12 @@
 /*   By: sevyesil <sevyesil@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/28 20:32:10 by sevyesil          #+#    #+#             */
-/*   Updated: 2026/04/28 20:44:36 by sevyesil         ###   ########.fr       */
+/*   Updated: 2026/04/28 23:56:31 by sevyesil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include <limits.h>
 
 void	set_cost(t_node *a, t_node *b)
 {
